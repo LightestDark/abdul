871870facdec3bd1bdd4ac895c84390dbba14a61
Abdul Wase Khawar <151097700+LightestDark@users.noreply.github.com>
2026-04-13 22:50:27 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/website',
-  assetPrefix: '/website/',
-};
+  basePath: '/abdul',
+  images: { unoptimized: true },
+}
 
-export default nextConfig;
+export default nextConfig
